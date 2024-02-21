cab620de09bd204ca80e9f6f906831dc96cf5f99
EmilyKang-77 <emilykang135@gmail.com>
2024-02-20 18:39:16 -0800

    add a navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="bg-background-color rounded border-black md:w-90% md:h-10%">navbar</div>
+  )
+}
+
+export default Navbar;
