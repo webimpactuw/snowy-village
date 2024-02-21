fa676bdbbf6204a5f92f41f83290a19d3e3367bc
Noah Huck <119907714+noahh0@users.noreply.github.com>
2024-02-20 20:37:08 -0800

    remove unused imports and comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React, { useState } from 'react'
-// import Navbar from './components/Navbar';
+import { FAQ } from "./pages/Contact/FAQ";
+import { Home } from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
-import { FAQ } from './pages/Contact/FAQ';
-import { Home } from './pages/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Home />} />
       <Route path="/faq" element={<FAQ />} />
-      <Route path="/collab" element={<Collab />}/>
-      <Route path="/hiring" element={<Hiring />}/>
+      <Route path="/collab" element={<Collab />} />
+      <Route path="/hiring" element={<Hiring />} />
 
-      <Route path="/gallery" element={<Gallery />}/>
-      <Route path="/menu" element={<Menu />}/>
-      
-      <Route path="/history" element={<History />}/>
-      <Route path="/locations" element={<Locations />}/> 
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/menu" element={<Menu />} />
+
+      <Route path="/history" element={<History />} />
+      <Route path="/locations" element={<Locations />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
