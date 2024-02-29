cbaf1c052513c619b58ecd5d87b024891285684c
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-02-28 22:51:18 -0800

    Add page links and a little styling
    Added some flex.
    Problem: navbar is overlapping with page content.
    Closes #10

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
-      <div className="bg-background-color rounded border-black md:w-90% md:h-10%">
-        navbar
+    <header className="flex justify-between items-center h-18 mx-auto fixed top-0 w-full px-4 bg-background-color">
+      <div className="rounded border-black">
+        <h1 className="text-2xl w-full">Snowy Village</h1>
+        <ul className="flex">
+          <li><Link to="/" className="p-4">Home</Link></li>
+          <li><Link to="/menu" className="p-4">Menu</Link></li>
+          <li><Link to="/gallery" className="p-4">Gallery</Link></li>
+          <li><Link to="/contact" className="p-4">Contact</Link></li>
+        </ul>
       </div>
+      </header>
     </>
   );
 }
