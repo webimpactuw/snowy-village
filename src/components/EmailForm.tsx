8d272642260fc0bb93bf3b32ce8edd8d0366ba65
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-03-03 18:05:23 -0800

    Make form typeable
    By implementing state.

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,34 +1,44 @@
+import { useState } from "react";
+
 export default function EmailForm() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
     
+    const sendEmail = (): void => {
+      // TODO:
+    };
+
     return <main className="flex min-h-screen flex-col items-center p-24">
       <form className="flex flex-col items-center justify-center">
         <input
           type="text"
           placeholder="Name"
           className="p-2 mb-4 border-2 rounded w-96"
-          value={""}
-          // onChange={(e) => setName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
        
         <input
           type="email"
           placeholder="Email"
           className="p-2 mb-4 border-2 rounded w-96"
-          value={""}
-          // onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <textarea
           placeholder="Message"
           className="p-2 mb-4 border-2 rounded h-64 w-96"
-          value={""}
-          // onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="button"
-          className="bg-blue-500 text-white p-2 rounded disabled:opacity-50"
-          // onClick={sendEmail}
-          // disabled={!name || !email || !message}
+          className="bg-dark-navy text-white p-2 rounded disabled:opacity-50"
+          onClick={sendEmail}
+          disabled={!name || !email || !message}
         >
           Send
         </button>
