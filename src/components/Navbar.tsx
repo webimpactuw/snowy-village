0f205bbbaf1120e98e5866d633361ed5820c605f
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-03-30 20:52:33 -0700

    update sidebar in mobile view so it takes up whole screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,20 +22,24 @@ function Navbar() {
         </ul>
 
         <div onClick={sideNavClicked} className="md:hidden">
-          {isSidebarOpen ? <AiOutlineClose /> : <AiOutlineMenu size={20} />}
+          {isSidebarOpen ? <></> : <AiOutlineMenu size={30}/>}
         </div>
       </div>
       
-      <div className={isSidebarOpen ? "fixed left-0 top-0 h-full w-[80%] border-r border-r-gray-900 bg-background-color" : "fixed hidden"}>
+      <div className={isSidebarOpen ? "fixed left-0 top-0 h-full w-full border-r border-r-gray-900 bg-background-color z-[9999]" : "fixed hidden"}>
         <div className="m-10">
-          <h1 className="text-2xl w-full pb-3">Snowy Village</h1>
-          <ul className="uppercase text-md">
+          <ul className="uppercase text-lg">
             <li className="pt-4 pb-2 border-b border-gray-600"><Link to="/" onClick={sideNavClicked}>Home</Link></li>
             <li className="pt-4 pb-2 border-b border-gray-600"><Link to="/menu" onClick={sideNavClicked}>Menu</Link></li>
             <li className="pt-4 pb-2 border-b border-gray-600"><Link to="/gallery" onClick={sideNavClicked}>Gallery</Link></li>
             <li className="pt-4 pb-2 border-b border-gray-600"><Link to="/contact" onClick={sideNavClicked}>Contact</Link></li>
           </ul>
         </div>
+
+        <div onClick={sideNavClicked} className="fixed top-2 right-2 md:hidden">
+          {isSidebarOpen ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30} />}
+        </div>
+
       </div>
     </>
   );
