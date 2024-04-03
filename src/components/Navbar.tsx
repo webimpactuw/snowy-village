36f8cb9ff693ed30bff1d8bfd6ca1074de2a0c52
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-04-02 19:29:23 -0700

    small changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,9 +22,9 @@ function Navbar() {
 
   return (
     <>
-        <div className={isSidebarOpen ? "hidden" : "sticky top-0 flex z-40"}>
-          <div className="bg-background-color sticky top-0 flex justify-between items-center h-2/10 mx-auto w-full px-4 rounded border-black">
-            <h1 className="text-2xl w-full">Snowy Village</h1>
+        <div className={isSidebarOpen ? "hidden" : "sticky top-0 flex z-40 bg-dark-navy text-white py-1 px-4"}>
+          <div className="sticky top-0 flex justify-between items-center mx-auto w-full border-black">
+            <h1 className="sm:hidden md:text-2xl w-full">Snowy Village</h1>
             <ul className="hidden md:flex">
               <li><Link to="/" className="p-4">Home</Link></li>
               <li><Link to="/menu" className="p-4">Menu</Link></li>
@@ -32,7 +32,7 @@ function Navbar() {
               <li><Link to="/contact" className="p-4">Contact</Link></li>
             </ul>
 
-            <div onClick={sideNavClicked} className="md:hidden top-2">
+            <div onClick={sideNavClicked} className="md:hidden top-2 right-1">
               <AiOutlineMenu size={30}/>
             </div>
           </div>
