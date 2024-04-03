89dc946981e8a40a78db16e7eb1cfaffc99c1354
Noah Huck <119907714+noahh0@users.noreply.github.com>
2024-04-03 01:05:38 -0700

    start main part of menu

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,10 +1,12 @@
 import MenuHeader from "./MenuHeader";
+import MenuBody from "./MenuBody";
 // import menuData from "./menu-data.json";
 
 export default function Menu() {
   return (
     <div>
       <MenuHeader />
+      <MenuBody />
     </div>
   );
 }
