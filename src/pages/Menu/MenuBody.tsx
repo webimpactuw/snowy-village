776baff48f87d34feddc9193651c8b01e980be04
Noah Huck <119907714+noahh0@users.noreply.github.com>
2024-04-03 22:37:46 -0700

    add rest of menu sections

--- src/pages/Menu/MenuBody.tsx
+++ src/pages/Menu/MenuBody.tsx
@@ -8,19 +8,23 @@ export default function MenuBody() {
     <div className="bg-ice-blue grid justify-center">
       <div className="grid grid-flow-col m-1 justify-center">
         <div className="flex items-center justify-center w-fit p-2">
-          <HeartSVG width="4" />
+          <HeartSVG width="3" />
           <p className="text-center text-dark-blue font-fjalla-one text-2xs tracking-widest font-thin p-1">
             = popular
           </p>
         </div>
         <div className="flex items-center justify-center w-fit p-2">
-          <NutSVG width="4" />
+          <NutSVG width="3" />
           <p className="text-center text-dark-blue font-fjalla-one text-2xs tracking-widest font-thin p-1">
             = contains nuts
           </p>
         </div>
       </div>
-      <MenuBlock name="BINGSOO" blockData={menuData.Bingsoo} />
+      <div className="grid gap-y-4">
+        <MenuBlock name="BINGSOO" blockData={menuData.bingsoo} />
+        <MenuBlock name="TAIYAKI" blockData={menuData.taiyaki} />
+        <MenuBlock name="DRINKS" blockData={menuData.drinks} />
+      </div>
     </div>
   );
 }
