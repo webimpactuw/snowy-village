7c4c7b3d46fcf58b6d490b0b20a968f7ae958d55
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-04-04 18:32:53 -0700

    Clean up code

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import EmailForm from "../../components/EmailForm";
 import SampleText from "../../components/SampleText";
-import HeroImage from "./contact.jpg";
-import Navbar from "../../components/Navbar";
 
 export default function Contact() {
 
-  const location: string = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2686.8333483101565!2d-122.31545112299888!3d47.6682337837186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549015057c7a99b3%3A0x47db1e5bf87c1c20!2sSnowy%20Village!5e0!3m2!1sen!2sus!4v1709185060487!5m2!1sen!2sus";
+  // const location: string = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2686.8333483101565!2d-122.31545112299888!3d47.6682337837186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549015057c7a99b3%3A0x47db1e5bf87c1c20!2sSnowy%20Village!5e0!3m2!1sen!2sus!4v1709185060487!5m2!1sen!2sus";
   const [showForm, setShowForm] = useState(false);
   // const [formButtonText, setFormButtonText] = useState("Show contact form");
 
@@ -22,21 +20,12 @@ export default function Contact() {
   return <div className="flex-col items-center">
 
     <div>
-      <Navbar />
       <div className="bg-dark-navy p-5 grid justify-center">
         <h1 className="text-center text-white font-fjalla-one text-4xl md:text-6xl tracking-widest mb-1">
           CONTACT US
         </h1>
       </div>
     </div>
-    
-    
-    {/* Hero section */}
-    {/* <div className="flex w-full h-25 text-white mx-auto">
-      <img src={HeroImage} className="w-full bg-blend-darken" style={{filter: "brightness(40%)"}}/>
-      <h1 className="font-fjalla-one text-4xl p-3 md:text-6xl left-1/5">CONTACT US</h1>
-      
-    </div> */}
 
     <div className="flex flex-col w-full p-5">
       <h1>FAQ</h1>
@@ -73,12 +62,8 @@ export default function Contact() {
       Address: 5264 University Way NE, Seattle, WA 98105
       </div>
 
-      <iframe src={location} className="w-full aspect-square md:w-1/3"></iframe>
+      {/* <iframe src={location} className="w-full aspect-square md:w-1/3"></iframe> */}
     </div>
-    
-   
-
-  
 
   </div>;
 }
