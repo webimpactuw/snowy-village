f1973159751d6b330a38a1e47e51e971fae3bcd8
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-04-14 22:16:31 -0700

    initial commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return(
+    <div className="bg-dark-blue p-5 grid justify-center">
+        <h1 className="text-center text-white font-fjalla-one text-5xl md:text-6xl tracking-widest mb-1">
+          Welcome to Snowy Village
+        </h1>
+      </div>
+  );
 }
