4c6ccc992ec94fc435c126aaf8e84e7ff43896b7
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-04-16 18:02:27 -0700

    change home and nav a little

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,31 @@
+import heroImage from "./image.png"
 export default function Home() {
   return(
-    <div className="bg-dark-blue p-5 grid justify-center">
-        <h1 className="text-center text-white font-fjalla-one text-5xl md:text-6xl tracking-widest mb-1">
-          Welcome to Snowy Village
+    <>
+    <div className="p-20 grid justify-center">
+        <h1 className="text-center text-black text-4xl md:text-5xl mb-1 font-bold">
+          Welcome to Snowy Village.
         </h1>
-      </div>
+
+        <h3>
+          We are a locally-owned Korean cafe, "text here"
+        </h3>
+
+    </div>
+
+    <div className="p-20 grid justify-center">
+      <h1 className="text-center text-black text-3xl md:text-4xl mb-1 font-bold">
+          We make the best bingsoo!
+      </h1>
+
+      <h3>
+        Bingsoo is shaved ice, flavored with "text here"
+      </h3>
+
+      <img src={heroImage}>
+
+      </img>
+    </div>
+    </>
   );
 }
