a995ed09d3401d9d87985d213ee13354f30da674
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-05-02 19:46:37 -0700

    adjust contact stuff

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,7 @@ import EmailForm from "../../components/EmailForm";
 import SampleText from "../../components/SampleText";
 import HiringImage from "./Hiring.jpg";
 import CollabImage from "./Collab.jpg";
+import Header from "./Header";
 
 export default function Contact() {
 
@@ -21,13 +22,9 @@ export default function Contact() {
 
   return <div className="flex-col items-center bg-ice-blue">
 
-      <div className="bg-dark-blue p-5 grid justify-center">
-        <h1 className="text-center text-white font-fjalla-one text-5xl md:text-6xl tracking-widest mb-1">
-          CONTACT US
-        </h1>
-      </div>
+      <Header />
 
-      <div className="flex flex-col w-full md:pl-[20%] md:pr-[20%] md:p-8">
+      <div className="flex flex-col w-full md:pl-[20%] md:pr-[20%] md:p-8 bg-beige">
         <div className="flex">
           <img src={HiringImage} className="w-1/2 p-3"/>
           <div className="p-3">
@@ -40,7 +37,7 @@ export default function Contact() {
         <button>Apply Now</button>
       </div>
 
-      <div className="flex flex-col w-full p-5 bg-dark-blue text-white md:pl-[20%] md:pr-[20%] md:p-8">
+      <div className="flex flex-col w-full p-5 bg-ice-blue md:pl-[20%] md:pr-[20%] md:p-8">
         <h1>Collab</h1>
         <img src={CollabImage} className="w-1/2 p-3"/>
         <SampleText />
@@ -59,7 +56,7 @@ export default function Contact() {
 
       </div>
 
-      <div className="flex flex-col w-full p-5 bg-ice-blue md:pl-[20%] md:pr-[20%] md:p-8">
+      <div className="flex flex-col w-full p-5 bg-beige md:pl-[20%] md:pr-[20%] md:p-8">
           <h1>FAQ</h1>
           <div>
             <SampleText />
---
+++ src/pages/Contact/Header.tsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <>
+      <div>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap');
+        </style>
+      </div>
+      <div className="container mx-auto h-16 w-3/4 pt-20">
+          <h1 className="text-aquamarine-blue font-fjalla-one font-bold text-center text-5xl">CONTACT US</h1>
+      </div>
+      <div className="mx-auto px-20 pt-20 pb-5">
+        <img src='src/assets/GalleryImages/border.png' alt='border'/>
+      </div>
+      {/* <div className="mx-auto text-center pb-20">
+        <h2 className="" style={{ fontFamily: "'Oooh Baby', cursive", color: '#00729D' }} >from the village, made with care</h2>
+      </div> */}
+    </>
+  )
+}
+
+export default Header;
