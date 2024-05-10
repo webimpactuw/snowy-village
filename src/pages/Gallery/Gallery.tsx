798dfbfb28e27d0d7869f1da35d454eb8f194e18
lezhiliu <108645759+lezhimiffyliu@users.noreply.github.com>
2024-05-09 17:18:20 -0700

    style changes

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -5,13 +5,13 @@ import GalleryGrid from './GalleryGrid';
 const testURL = 'https://handletheheat.com/wp-content/uploads/2021/06/homemade-vanilla-ice-cream.jpg';
 
 const images = [
+  { src: 'src/assets/GalleryImages/image 72.png', alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
+  { src: 'src/assets/GalleryImages/image 80.png', alt: 'Description of image', caption: 'Strawberry' },
+  { src: 'src/assets/GalleryImages/image 82.png', alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
+  { src: 'src/assets/GalleryImages/image 106.png', alt: 'Description of image', caption: 'Strawberry' },
+  { src: 'src/assets/GalleryImages/image 110.png', alt: 'Description of image', caption: 'Strawberry' },
   { src: testURL, alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
-  { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
+  { src: 'src/assets/GalleryImages/image 82.png', alt: 'Description of image', caption: 'Strawberry' },
   { src: testURL, alt: 'Description of image', caption: 'Strawberry', contentType: 'featured' },
   { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
   { src: testURL, alt: 'Description of image', caption: 'Strawberry' },
@@ -26,9 +26,7 @@ function Gallery() {
       <div className="bg-background-color" style={{color: "F0FAFF"}}>
         <Header />
       </div>
-      {/* <div className="mx-auto bg-white">
-        <img src="src/assets/GalleryImages/DRIPPPPP.png" alt='dripping'/>
-      </div> */}
+      
       <div className='pb-20' style={{color: "FFFFFF"}}>
         <GalleryGrid images={images} />
       </div>
