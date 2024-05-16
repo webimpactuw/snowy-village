9185e74a1f6ff4d3ddfb7ae0fb9740969a15c976
lezhiliu <108645759+lezhimiffyliu@users.noreply.github.com>
2024-05-16 16:44:58 -0700

    header style

--- src/pages/AboutUs/AboutHeader.tsx
+++ src/pages/AboutUs/AboutHeader.tsx
@@ -1,29 +1,33 @@
 const AboutHeader = () => {
-    return (
-      <>
-        <div>
-          <style>
-            @import url('https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap');
-          </style>
+  return (
+    <>
+      <div>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap');
+        </style>
+      </div>
+      <div style={{ backgroundColor: '#E8F4FF' }}>
+        <div className="mx-auto px-20 pt-20 text-left flex">
+          <div className="flex-1">
+            <div className="container mx-auto h-16 py-20">
+              <h1 className="text-text-color-1 font-fjalla-one font-bold text-center text-5xl" style={{ color: '#00729D' }}>
+                ABOUT US
+              </h1>
+           </div>
+            {/* <img src="src/assets/GalleryImages/border.png" alt="border" /> */}
+            <h2 style={{ fontFamily: "'Oooh Baby', cursive", color: '#00729D' }}>
+              from the village, made with care
+            </h2>
+          </div>
+          <div className="flex-1">
+            <img src="src/assets/GalleryImages/DRIPPPPP-3.png" alt="image" />
+          </div>
         </div>
-        <div style={{ backgroundColor: '#E8F4FF' }}>
-            <div className="container mx-auto h-16 pt-20" >
-                <h1 className="text-text-color-1 font-fjalla-one font-bold text-center text-5xl" style={{ color: '#00729D' }}>ABOUT US</h1>
-            </div>
-            <div className="mx-auto px-20 pt-20">
-            <img src='src/assets/GalleryImages/border.png' alt='border'/>
-            </div>
-            <div className="mx-auto text-center pb-20">
-            <h2 className="" style={{ fontFamily: "'Oooh Baby', cursive", color: '#00729D' }} >from the village, made with care</h2>
-            </div>
-        </div>
-        {/* <div className="">
-            <img src="src/assets/GalleryImages/DRIPPPPP-3.png"/>
-        </div> */}
-
-        
-      </>
-    )
+        <div className="mx-auto text-center pb-20"></div>
+      </div>
+    </>
+  )
+  
   }
   
   
