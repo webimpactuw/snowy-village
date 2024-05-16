f28c40fb1aeda20c678a57c782e936d8899f6ebe
lezhiliu <108645759+lezhimiffyliu@users.noreply.github.com>
2024-05-16 00:36:08 -0700

    images change

--- src/pages/AboutUs/About.tsx
+++ src/pages/AboutUs/About.tsx
@@ -1,22 +1,20 @@
 import Locations from "./Locations";
+import AboutHeader from "./AboutHeader";
 
 const testURL = 'https://www.betterup.com/hs-fs/hubfs/Happy-work-team-cheering-and-celebrating-at-meeting-team-collaboration.jpg?width=1928&height=1286&name=Happy-work-team-cheering-and-celebrating-at-meeting-team-collaboration.jpg';
 
 export default function About() {
   return <>
-
+    <AboutHeader/>
     <span id="team"/>
-    <div className="mx-auto h-30 w-full pt-10 pb-10 bg-dark-blue">
-      <h1 className="text-text-color-2 font-fjalla-one text-3xl text-center">ABOUT US</h1>
-    </div>
 
-    <div className="mx-auto h-120 w-full md:px-20 sm:px-10 py-10 bg-white">
-      <div className=" bg-white pb-10">
+    <div className="mx-auto h-120 w-full md:px-20 sm:px-10 py-10" style={{ backgroundColor: '#E8F4FF' }}>
+      <div className=" pb-10">
         <h1 className="text-dark-blue font-fjalla-one text-3xl text-center">OUR TEAM</h1>
       </div>
       
-      <div className=" bg-white pb-20">
-      <div className="flex flex-col md:flex-row w-full h-auto bg-white p-5">
+      <div className=" pb-20" style={{ backgroundColor: '#E8F4FF' }}>
+      <div className="flex flex-col md:flex-row w-full h-auto p-5">
         <div className="flex-none w-full md:w-80 relative">
           <div className="md:flex-none w-full md:w-80 h-48 relative">
             <img src={testURL} alt="Team" className="absolute inset-0 w-full h-full object-cover" />
