003a56e8c62630aa6c2c9c831b965070a0ebc6de
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-05-19 14:50:45 -0700

    Try to get sanity studio to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+      input: ["./src/main.tsx"]
+    },
   }
 })
